c9a343f3bc55fdf9337bbf2dd300fbcd54ced9d1
ggq <ggq18663278150@sian.com>
2019-05-06 11:34:35 +0800

    添加了定时任务

--- Scrapy project/baidubaipin/baidubaipin/entrypoint.py
+++ Scrapy project/baidubaipin/baidubaipin/entrypoint.py
@@ -1,3 +1,28 @@
 from scrapy import cmdline
+from time import sleep, strftime, localtime, time
+import schedule
+import atexit
 
-cmdline.execute(['scrapy', 'crawl', 'bdbp'])
+
+@atexit.register
+def close_file():
+    print("trying to close file..")
+    if not f.closed:
+        f.close()
+
+
+f = open("log.log", encoding="utf-8", mode="a")
+
+
+def job():
+    f.writelines(f"scrapy start to crawl at {strftime('%Y.%m.%d %H:%M:%S', localtime(time()))}\n")
+    f.flush()
+    cmdline.execute(['scrapy', 'crawl', 'bdbp'])
+
+
+schedule.every().day.at("10:00").do(job)
+schedule.every().day.at("21:00").do(job)
+
+while True:
+    schedule.run_pending()
+    sleep(60)
